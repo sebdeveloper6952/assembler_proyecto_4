ca5778bd00e549bbdc577e63bd707a49c118e8c1
Sebastian Arriola <sebdeveloper6952@gmail.com>
2018-05-19 14:07:35 -0600

    Programar alarma por software.

--- alarma.s
+++ alarma.s
@@ -59,13 +59,46 @@ main:
   mov r0, #25
   mov r1, #0
   bl SetGpio
- 
+  
+  @configurar alarmas
+  ldr r0, =msgMenu
+  bl printf
+  ldr r0, =msgSoftware
+  bl printf
+  ldr r0, =msgBotones
+  bl printf
+  ldr r0, =msgIngreseOpcion
+  bl printf
+  ldr r0, addr_fmtInt
+  ldr r1, =opcionIngresada
+  bl scanf
+  ldr r0, =opcionIngresada
+  ldr r0, [r0]
+  cmp r0, #2
+  beq programar_por_botones
+  
+@programar por software
+programar_por_software:
+  ldr r0, =msgIngreseAlarma
+  bl printf
+  ldr r0, addr_fmtInt
+  ldr r1, addr_alarma0
+  bl scanf
+  b iniciar_alarma
+
+  @programar por botones
+  programar_por_botones:
+  
+  
   @prueba delay 1 segundo
-  mov r4, #0              @contador de alarma
+iniciar_alarma:
+  mov r4, #0                  @contador de alarma
+  ldr r7, addr_alarma0        @cargar alarma configurada por usuario
+  ldr r7, [r7]
   loop:
     bl wait1s             @espera de 1 segundo
     add r4, r4, #1        @incrementar contador de alarma
-    cmp r4, #100
+    cmp r4, #60
     moveq r4, #0
     mov r0, r4
     bl ObtenerDigitos     @r0 = digito 1; r1 = digito 2
@@ -79,21 +112,30 @@ main:
     bl EscribirDigito1
     mov r0, r6 
     bl EscribirDigito2
+    cmp r7, r4
+    beq sonar_alarma
   bal loop
+
+sonar_alarma:
+  ldr r0, =msgSonarAlarma
+  bl printf
   
 exit:
   mov r7, #1
   svc 0
 
+@subrutinas
 wait1s:
   push {r4, r5, lr}
   bl getCPS
   vmov s2, r0
-  vcvt.f32.s32 s2, s2 @ CPS
-  ldr r1, addr_floatOne
-  vldr s6, [r1]              @s6 = 1f
+  vcvt.f32.s32 s2, s2           @ CPS
+  ldr r1, addr_floatOne 
+  ldr r1, [r1]
+  vmov s6, r1                   @ s6 = 0.00000
   ldr r1, addr_floatZero
-  vldr s5, [r1]              @s5 = 0f
+  ldr r1, [r1]
+  vmov s5, r1                   @ s5 = 1.00000
   wait1s_loop:
     bl getCycles
     mov r4, r0
@@ -119,14 +161,20 @@ delay:
 
 addr_floatOne: .word floatOne
 addr_floatZero: .word floatZero
+addr_fmtInt: .word fmtInt
+addr_alarma0: .word alarma0
   
 .data
-  cps: .word 0
+.align 4
   num: .asciz "%d %d \n"
-  msgCPS: .asciz "CPS: %d\n"
-  time: .asciz "Time: %15.10f\n"
+  fmtInt: .asciz "%d"
+  opcionIngresada: .word 0
+  msgMenu: .asciz "***************** Menu ****************** \n"
+  msgSoftware: .asciz "1. Programar alarmas por medio del programa. \n"
+  msgBotones: .asciz "2. Programar alarmas por medio de botones. \n"
+  msgIngreseOpcion: .asciz "Ingrese opcion: \n"
+  msgIngreseAlarma: .asciz "Ingrese a que segundo suena la alarma (0 - 60): \n"
+  msgSonarAlarma: .asciz "DESPIEEEEEEERTEEEEEEESEEEEEEE!\n"
   floatOne: .float 1.000000
   floatZero: .float 0.000000
   alarma0: .word 0
-  alarma1: .word 0
-  alarma2: .word 0
